501841210128d95ce5a58e8179a8d1d3366c9e5a
chrlaney <claney@battelleecology.org>
2017-10-25 16:38:27 -0600

    replace table_types.rda with table_types.Rdata and document

--- neonDataStackR/R/stackDataFiles.R
+++ neonDataStackR/R/stackDataFiles.R
@@ -21,7 +21,7 @@
 
 stackDataFiles <- function(folder){
   # get the in-memory list of table types (site-date, site-all, etc.). This list must be updated often.
-  load("data/table_types.rda")
+  load("data/table_types.Rdata")
   ttypes <<- table_types
 
   # filenames without full path
---
+++ neonDataStackR/R/table_types.R
@@ -0,0 +1,13 @@
+#' Publication table information
+#'
+#' A dataset containing publication table names, descriptions, type (site-date, site-all, lab-all, lab-current), and a time index
+#'
+#' @format A data frame with 4 variables. Number of row changes frequently as more tables are added:
+#' \describe{
+#'   \item{tableName}{name of table}
+#'   \item{tableDesc}{description of table}
+#'   \item{tableType}{type of table (important for knowing which tables to stack and which to not stack)}
+#'   \item{tableTMI}{a time index (e.g., 1 = native resolution or 1 minute, 30 = 30 minute averages or totals)}
+#' }
+#' @source dps database
+"table_types"
