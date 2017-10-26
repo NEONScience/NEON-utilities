84dd1d38e171f9d8179614846df8d31a52f6d484
chrlaney <claney@battelleecology.org>
2017-10-26 14:18:12 -0600

    table_types

--- neonDataStackR/R/update_table_types.R
+++ neonDataStackR/R/update_table_types.R
@@ -21,5 +21,5 @@ update_table_types <- function(){
   library(RMySQL)
   dps_con <-  dbConnect(drv=RMySQL::MySQL(), dbname="dps_database", username="root",password="p@ssw0rd", host="10.206.27.43", root = 3306)
   table_types <- dbGetQuery(dps_con, "SELECT * from dpTable WHERE dpTable.tableType != 'ingest'")
-  save(table_types, file = "data/table_types.Rdata")
+  save(table_types, file = "data/table_types.rda")
 }
