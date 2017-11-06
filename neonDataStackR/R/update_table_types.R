32936474cc98a879c5c0d9a2f9a8c6f55a47f2a2
chrlaney <claney@battelleecology.org>
2017-11-06 15:09:11 -0700

    remove L0prime tables

--- neonDataStackR/R/update_table_types.R
+++ neonDataStackR/R/update_table_types.R
@@ -20,6 +20,7 @@
 update_table_types <- function(){
   library(RMySQL)
   dps_con <-  dbConnect(drv=RMySQL::MySQL(), dbname="dps_database", username="root",password="p@ssw0rd", host="10.206.27.43", root = 3306)
-  table_types <- dbGetQuery(dps_con, "SELECT * from dpTable WHERE dpTable.tableType != 'ingest'")
+  table_types <- dbGetQuery(dps_con, "SELECT * from dpTable WHERE dpTable.tableType NOT IN ('ingest', 'L0prime')")
   save(table_types, file = "data/table_types.rda")
+  dbDisconnect(dps_database)
 }
