df5e32e5fa7175ba83c15178687d8a5fa26b9a8d
cklunch <clunch@neoninc.org>
2018-04-09 13:21:48 -0600

    install alert on neonDataStackR

---
+++ neonDataStackR/R/zzz.R
@@ -0,0 +1,4 @@
+.onLoad <- function(libname, pkgname) {
+  packageStartupMessage('The neonDataStackR package is deprecated, and its functions have moved to the neonUtilities package. github.com/NEONScience/NEON-utilities/neonUtilities  Update your code and paths!')
+}
+.onLoad()
