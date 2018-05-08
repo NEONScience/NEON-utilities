292d00ecdda89129988bde62a3c23751f4f6dbe6
cklunch <clunch@neoninc.org>
2018-05-08 16:30:48 -0600

    added averaging interval option to zipsByProduct
    @chrlaney

--- neonUtilities/R/zipsByProduct.R
+++ neonUtilities/R/zipsByProduct.R
@@ -10,6 +10,7 @@
 #' @param dpID The identifier of the NEON data product to pull, in the form DPL.PRNUM.REV, e.g. DP1.10023.001
 #' @param site Either the string 'all', or the four-letter code of a single NEON site, e.g. 'CLBJ'. Future versions may allow more options for subsetting than one or all sites. Defaults to all.
 #' @param package Either 'basic' or 'expanded', indicating which data package to download. Defaults to basic.
+#' @param avg Either the string 'all', or the averaging interval to download, in minutes. Only applicable to sensor (IS) data. Defaults to 'all'.
 #' @param check.size T or F, should the user be told the total file size before downloading? Defaults to T. When working in batch mode, or other non-interactive workflow, use check.size=F.
 
 #' @return A folder in the working directory, containing all zip files meeting query criteria.
@@ -35,7 +36,7 @@ zipsByProduct <- function(dpID, site="all", package="basic", avg="all", check.si
   if(regexpr("DP[1-4]{1}.[0-9]{5}.001",dpID)!=1) {
     stop(paste(dpID, "is not a properly formatted data product ID. The correct format is DP#.#####.001", sep=" "))
   }
-
+  
   # query the products endpoint for the product requested
   productUrl <- paste0("http://data.neonscience.org/api/v0/products/", dpID)
   req <- httr::GET(productUrl)
@@ -46,6 +47,24 @@ zipsByProduct <- function(dpID, site="all", package="basic", avg="all", check.si
     stop(paste("No data found for product", dpID, sep=" "))
   }
 
+  # error message if averaging interval is invalid
+  if(avg!="all") {
+    # if product is OS, proceed with normal download
+    if(avail$data$productScienceTeamAbbr %in% c("TOS","AOS") | 
+       dpID %in% c("DP1.20267.001","DP1.00101.001","DP1.00013.001","DP1.00038.001")) {
+      messages <- c(messages, paste(dpID, "is an observational product; 
+                                    averaging interval is not a download option. 
+                                    Downloading all zip files.", sep=" "))
+  } else {
+    # check and make sure the averaging interval is valid for the product
+    if(!avg %in% table_types$tableTMI[which(table_types$productID==dpID)]) {
+      stop(paste(avg, " is not a valid averaging interval for ", dpID, 
+                 ". Use function getAvg() to find valid averaging intervals.", sep=""))
+    }
+  }
+    }
+  
+  
   # get the urls for months with data available
   month.urls <- unlist(avail$data$siteCodes$availableDataUrls)
 
@@ -60,7 +79,7 @@ zipsByProduct <- function(dpID, site="all", package="basic", avg="all", check.si
   if(length(month.urls)==0) {
     stop("There are no data at the selected site.")
   }
-
+  
   # get all the file names, and stash the URLs for just the zips in an object
   zip.urls <- c(NA, NA, NA)
   messages <- NA
@@ -76,58 +95,92 @@ zipsByProduct <- function(dpID, site="all", package="basic", avg="all", check.si
       next
     }
     
-    # if only one averaging interval, subset to those
+    # if only one averaging interval is requested, divert to filesByAvg()
     if(avg!="all") {
       
-      # check, if this is an OS product, proceed with normal zip download
-      if(avail$data$productScienceTeamAbbr %in% c("TOS","AOS") | 
-         dpID %in% c("DP1.20267.001","DP1.00101.001","DP1.00013.001","DP1.00038.001")) {
-        messages <- c(messages, paste(dpID, "is an observational product; 
-                                      averaging interval is not a download option. 
-                                      Downloading all zip files.", sep=" "))
-      } else {
-        # I'm thinking send it to a new function here?
+      # select files by averaging interval
+      all.file <- grep(paste(avg, "min", sep=""), tmp.files$data$files$name, fixed=T)
+      
+      if(length(all.file)==0) {
+        messages <- c(messages, paste("No files found for site", tmp.files$data$siteCode,
+                                      "and month", tmp.files$data$month, sep=" "))
+        next
       }
-    }
-
-    # to get all data, select the zip files
-    all.zip <- grep(".zip", tmp.files$data$files$name, fixed=T)
-
-    # error message if there are no zips in the package
-    if(length(all.zip)==0) {
-      messages <- c(messages, paste("No zip files found for site", tmp.files$data$siteCode,
-                 "and month", tmp.files$data$month, sep=" "))
-      next
-    }
-
-    # if package==expanded, check that expanded package exists
-    # if it doesn't, download basic package
-    pk <- package
-    if(pk=="expanded") {
-      if(length(grep(pk, tmp.files$data$files$name))==0) {
-        pk <- "basic"
-        messages <- c(messages, paste("No expanded package found for site ",
-                                      tmp.files$data$siteCode, " and month ",
-                                      tmp.files$data$month,
-                                      ". Basic package downloaded instead.",
-                                      sep=""))
+      
+      # if package==expanded, check that expanded package exists
+      # if it doesn't, download basic package
+      pk <- package
+      if(pk=="expanded") {
+        if(length(grep(pk, tmp.files$data$files$name))==0) {
+          pk <- "basic"
+          messages <- c(messages, paste("No expanded package found for site ",
+                                        tmp.files$data$siteCode, " and month ",
+                                        tmp.files$data$month,
+                                        ". Basic package downloaded instead.",
+                                        sep=""))
+        }
       }
+      
+      # subset to package
+      which.file <- intersect(grep(pk, tmp.files$data$files$name, fixed=T),
+                             grep(paste(avg, "min", sep=""), tmp.files$data$files$name, fixed=T))
+      
+      # check again for no files
+      if(length(which.file)==0) {
+        messages <- c(messages, paste("No basic package files found for site",
+                                      tmp.files$data$siteCode,
+                                      "and month", tmp.files$data$month, sep=" "))
+        next
+      }
+      
+      zip.urls <- rbind(zip.urls, cbind(tmp.files$data$files$name[which.file],
+                                        tmp.files$data$files$url[which.file],
+                                        tmp.files$data$files$size[which.file]))
+      
+    } else {
+      
+      # to get all data, select the zip files
+      all.zip <- grep(".zip", tmp.files$data$files$name, fixed=T)
+      
+      # error message if there are no zips in the package
+      if(length(all.zip)==0) {
+        messages <- c(messages, paste("No zip files found for site", tmp.files$data$siteCode,
+                                      "and month", tmp.files$data$month, sep=" "))
+        next
+      }
+      
+      # if package==expanded, check that expanded package exists
+      # if it doesn't, download basic package
+      pk <- package
+      if(pk=="expanded") {
+        if(length(grep(pk, tmp.files$data$files$name))==0) {
+          pk <- "basic"
+          messages <- c(messages, paste("No expanded package found for site ",
+                                        tmp.files$data$siteCode, " and month ",
+                                        tmp.files$data$month,
+                                        ". Basic package downloaded instead.",
+                                        sep=""))
+        }
+      }
+      
+      # subset to package
+      which.zip <- intersect(grep(pk, tmp.files$data$files$name, fixed=T),
+                             grep(".zip", tmp.files$data$files$name, fixed=T))
+      
+      # check again for no files
+      if(length(which.zip)==0) {
+        messages <- c(messages, paste("No basic package files found for site",
+                                      tmp.files$data$siteCode,
+                                      "and month", tmp.files$data$month, sep=" "))
+        next
+      }
+      
+      zip.urls <- rbind(zip.urls, cbind(tmp.files$data$files$name[which.zip],
+                                        tmp.files$data$files$url[which.zip],
+                                        tmp.files$data$files$size[which.zip]))
+      
     }
 
-    which.zip <- intersect(grep(pk, tmp.files$data$files$name, fixed=T),
-                           grep(".zip", tmp.files$data$files$name, fixed=T))
-
-    # check again for no files
-    if(length(which.zip)==0) {
-      messages <- c(messages, paste("No basic package files found for site",
-                                    tmp.files$data$siteCode,
-                                    "and month", tmp.files$data$month, sep=" "))
-      next
-    }
-
-    zip.urls <- rbind(zip.urls, cbind(tmp.files$data$files$name[which.zip],
-                                      tmp.files$data$files$url[which.zip],
-                                      tmp.files$data$files$size[which.zip]))
   }
 
   # get size info
@@ -156,7 +209,7 @@ zipsByProduct <- function(dpID, site="all", package="basic", avg="all", check.si
     downloader::download(zip.urls$URL[i], paste(filepath, zip.urls$name[i], sep="/"), mode="wb")
   }
 
-  messages <- c(messages, paste(nrow(zip.urls)-1, "zip files downloaded to",
+  messages <- c(messages, paste(nrow(zip.urls)-1, "files downloaded to",
                                 filepath, sep=" "))
   writeLines(paste0(messages[-1], collapse = "\n"))
 
