93e4e2e5f907d2d3a879c5573e6fdd1a14cbfffc
chrlaney <claney@battelleecology.org>
2018-09-25 13:57:44 -0600

    Delete old neonDataStackR code - has been incorporated and unpdated in neonUtilities.

--- neonDataStackR/R/zzz.R
+++
@@ -1,4 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-  packageStartupMessage('The neonDataStackR package is deprecated, and its functions have moved to the neonUtilities package. github.com/NEONScience/NEON-utilities/neonUtilities  Update your code and paths!')
-}
-.onLoad()
