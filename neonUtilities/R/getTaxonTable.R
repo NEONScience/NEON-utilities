b23d4333d04e7e764b6feee482a66ae9acaa7577
Eric R. Sokol <esokol@battelleecology.org>
2019-01-10 15:29:25 -0700

    correct spelling

--- neonUtilities/R/getTaxonTable.R
+++ neonUtilities/R/getTaxonTable.R
@@ -10,17 +10,17 @@
 #'
 #' @import httr jsonlite
 #'
-#' @param taxonType Character string for the taxonTypeCode. Must be one of ALGAE, GEETLE, BIRD,
-#' FISH, HERPETOLOY, MACROINVERTEBRATE, MOSQUITO, MOSQUITO_PATHOGENS, SMALL_MAMMAL, PLANT, TICK
+#' @param taxonType Character string for the taxonTypeCode. Must be one of ALGAE, BEETLE, BIRD,
+#' FISH, HERPETOLOGY, MACROINVERTEBRATE, MOSQUITO, MOSQUITO_PATHOGENS, SMALL_MAMMAL, PLANT, TICK
 #' @param recordReturnLimit Integer. The number of items to limit the result set to. If NA, will return all records in table.
-#' @param stream Character string. Option to obtain the result as a stream. Utilize for large requests.
+#' @param stream Character string, true or false. Option to obtain the result as a stream. Utilize for large requests.
 #'
 #' @return data frame with selected NEON data
 #'
 #'
 #' @examples
 #' # taxonTypeCode must be one of
-#' # ALGAE, GEETLE, BIRD, FISH,
+#' # ALGAE, BEETLE, BIRD, FISH,
 #' # HERPETOLOGY, MACROINVERTEBRATE, MOSQUITO, MOSQUITO_PATHOGENS,
 #' # SMALL_MAMMAL, PLANT, TICK
 #' #################################
