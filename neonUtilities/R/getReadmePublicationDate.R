2c1473147340d5dcaf320708e4cf25f5b79d338e
NateMietk <natemietkiewicz@gmail.com>
2019-10-21 16:30:59 -0600

    first commit

---
+++ neonUtilities/R/getReadmePublicationDate.R
@@ -0,0 +1,79 @@
+##############################################################################################
+#' @title Scrape the publication date from each ReadMe file
+
+#' @author
+#' Nathan Mietkiewicz \email{mietkiewicz@battelleecology.org}
+
+#' @description
+#' Given a directory, this will recursively list all of the ReadMe files that were unzipped.
+#' This should result in a single text file with a list of all of the publication dates from the ReadMe file.
+
+#' @param savepath The root folder directory where the ReadMe files are located.
+#' @param out_filepath_name The output directory and filename.  
+
+#' @examples
+#' \dontrun{
+#' # To unzip and merge files downloaded from the NEON Data Portal
+#' stackByTable("~/NEON_par.zip")
+#' 
+#' # To unzip and merge files downloaded using zipsByProduct()
+#' stackByTable("~/filesToStack00024", folder=T)
+#' }
+
+#' @export
+
+#' @references
+#' License: GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007
+
+# Changelog and author contributions / copyrights
+#   2019-10-08 (Nathan Mietkiewicz): Original creation
+
+##############################################################################################
+
+getReadmePublicationDate <- function(savepath, out_filepath) {
+    
+  readme_list <- list.files(savepath, pattern = 'readme',
+                            recursive = TRUE, full.names = TRUE)
+  
+  pub_date_df <- do.call(rbind, pblapply(readme_list, function(x) {
+    split <- x %>%
+      stringr::str_split(., '/') %>%
+      unlist() %>%
+      .[max(length(unlist(.)))] 
+    splitter <- split %>%
+      stringr::str_split(., '\\.') %>%
+      unlist()
+    
+    pub_date_str <- suppressWarnings(
+      suppressMessages(readr::read_csv(x, col_names=c('X1', 'X2')) %>%
+                         dplyr::filter(str_detect(X1, 'Date-Time for Data Publication'))))
+    
+    tmp_pub_date_df <- pub_date_str %>%
+      dplyr::mutate(publication_date = lubridate::ymd_hm(stringr::str_remove(.$X1, 'Date-Time for Data Publication: ')),
+                    domain= as_factor(splitter[2]),
+                    site = as_factor(splitter[3]),
+                    dp_id = as_factor(paste(splitter[4:6], collapse = '.')), 
+                    readme_filename = as_factor(split)) %>%
+      dplyr::select(-X1, -X2)
+
+    return(tmp_pub_date_df)
+  }))
+  
+  txt_file <- readr::read_lines(readme_list[[max(length(readme_list))]]) %>%
+    gsub('Date-Time for Data Publication: [[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}[[:space:]]{1}[[:digit:]]{2}:[[:digit:]]{2} ', 
+         'Date-Time for Data Publication: Because this is the compiled version, you will find these dates at the end of the document.', .)
+
+  out_filepath_name <- paste0(out_filepath, '/stacked_compiled_readme_', format(Sys.time(), "%Y%m%d%H%M%S"), '.txt')
+  
+  write_lines(txt_file, out_filepath_name)
+  cat("\n", file = out_filepath_name, append=TRUE)
+  cat("\n", file = out_filepath_name, append=TRUE)
+  cat("POST STACKING README DOCUMENTATION\n", file = out_filepath_name, append=TRUE)
+  cat("----------------------------------\n", file = out_filepath_name, append=TRUE)
+  cat("\n", file = out_filepath_name, append=TRUE)
+  cat("Compiled list of the data publication record of the files stacked.\nEach row contains information specific to a layer in the stack.\n\nFrom left to right, the publication date, domain, site, data product ID, and original readme filenames are listed.\n\n", 
+      file = out_filepath_name, append=TRUE)
+  write.table(pub_date_df, file=out_filepath_name, 
+              sep=",", append=TRUE, row.names=FALSE, col.names=FALSE, quote = FALSE)
+  
+}
