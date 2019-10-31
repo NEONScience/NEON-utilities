5fae9f860d74124712740966c732f0b6b6380036
NateMietk <natemietkiewicz@gmail.com>
2019-10-31 15:46:32 -0600

    reduced the amount of object objects to readme, added disclaimer, and quiet namespaces

--- neonUtilities/R/getReadmePublicationDate.R
+++ neonUtilities/R/getReadmePublicationDate.R
@@ -31,9 +31,9 @@
 ##############################################################################################
 
 getReadmePublicationDate <- function(savepath, out_filepath) {
-  requireNamespace('dplyr')
-  requireNamespace('magrittr')
-  requireNamespace('stringr')
+  requireNamespace('dplyr', quiet=TRUE)
+  requireNamespace('magrittr', quiet=TRUE)
+  requireNamespace('stringr', quiet=TRUE)
   
   out_filepath_name <- paste0(out_filepath, '/readme.txt')
 
@@ -49,34 +49,28 @@ getReadmePublicationDate <- function(savepath, out_filepath) {
         stringr::str_split(., '/') %>%
         unlist() %>%
         .[max(length(unlist(.)))] 
-      splitter <- split %>%
-        stringr::str_split(., '\\.') %>%
-        unlist()
       
       pub_date_str <- suppressWarnings(
         suppressMessages(readr::read_csv(x, col_names=c('X1', 'X2')) %>%
-                           dplyr::filter(stringr::str_detect(X1, 'Date-Time for Data Publication'))))
-
-      tmp_pub_date_df <- pub_date_str %>%
-        dplyr::mutate(publication_date = lubridate::ymd_hm(stringr::str_remove(X1, 'Date-Time for Data Publication: ')),
-                      domain= as.factor(splitter[2]),
-                      site = as.factor(splitter[3]),
-                      dp_id = as.factor(paste(splitter[4:6], collapse = '.')), 
-                      readme_filename = as.factor(split)) %>%
+                           dplyr::filter(stringr::str_detect(X1, 'Date-Time for Data Publication')))) %>%
+        dplyr::mutate(readme_filename = as.factor(split)) %>%
         dplyr::select(-X1, -X2)
       
-      return(tmp_pub_date_df)
+      return(pub_date_str)
     }))
     txt_file <- readr::read_lines(readme_list[[max(length(readme_list))]])
     
-    readr::write_lines(txt_file, out_filepath_name)
+    cat("##################################\n", file = out_filepath_name)
+    cat("########### Attention ############\n", file = out_filepath_name, append=TRUE)
+    cat('Disclaimer: this is a compiled version and may have some information from specific site-dates based on readme files used during stackByTable.\n', file = out_filepath_name, append=TRUE)
+    cat("##################################\n", file = out_filepath_name, append=TRUE)
     cat("\n", file = out_filepath_name, append=TRUE)
+    readr::write_lines(txt_file, out_filepath_name, append=TRUE)
     cat("\n", file = out_filepath_name, append=TRUE)
     cat("POST STACKING README DOCUMENTATION\n", file = out_filepath_name, append=TRUE)
     cat("----------------------------------\n", file = out_filepath_name, append=TRUE)
     cat("\n", file = out_filepath_name, append=TRUE)
-    cat("Compiled list of the data publication record of the files stacked.\nEach row contains information specific to a layer in the stack.\n\nFrom left to right, the publication date, domain, site, data product ID, and original readme filenames are listed.\n\n", 
-        file = out_filepath_name, append=TRUE)
-    utils::write.table(pub_date_df, file=out_filepath_name, 
-                sep=",", append=TRUE, row.names=FALSE, col.names=FALSE, quote = FALSE)
+    cat("Each row contains the readme filename used during stackByTable\n", file = out_filepath_name, append=TRUE)
+    cat("\n", file = out_filepath_name, append=TRUE)
+    utils::write.table(pub_date_df, file=out_filepath_name, sep=",", append=TRUE, row.names=FALSE, col.names=FALSE, quote = FALSE)
 }
