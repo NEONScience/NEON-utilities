a53130ec5b48030bb28e0e1179a4e93420271c9d
NateMietk <natemietkiewicz@gmail.com>
2019-11-25 10:22:23 -0700

    remove Date-Time from compiled readme

--- neonUtilities/R/getReadmePublicationDate.R
+++ neonUtilities/R/getReadmePublicationDate.R
@@ -25,41 +25,42 @@ getReadmePublicationDate <- function(savepath, out_filepath) {
   requireNamespace('stringr', quiet=TRUE)
   
   out_filepath_name <- paste0(out_filepath, '/readme.txt')
+  
+  if(file.exists(out_filepath_name)) {
+    unlink(out_filepath_name)
+  }
+  #writeLines("Stacking ReadMe documentation")
+  readme_list <- list.files(savepath, pattern = '.readme.',
+                            recursive = TRUE, full.names = TRUE)
+  
+  pub_date_df <- do.call(rbind, pbapply::pblapply(readme_list, function(x) {
+    split <- x %>%
+      stringr::str_split(., '/') %>%
+      unlist() %>%
+      .[max(length(unlist(.)))]
 
-    if(file.exists(out_filepath_name)) {
-      unlink(out_filepath_name)
-    }
-    #writeLines("Stacking ReadMe documentation")
-    readme_list <- list.files(savepath, pattern = '.readme.',
-                              recursive = TRUE, full.names = TRUE)
+    pub_date_str <- suppressWarnings(
+      suppressMessages(readr::read_csv(x, col_names=c('X1', 'X2')))) %>%
+      dplyr::mutate(readme_filename = as.factor(split)) %>%
+      dplyr::select(-c('X1', 'X2'))
     
-    # pub_date_df <- do.call(rbind, pbapply::pblapply(readme_list, function(x) {
-    #   split <- x %>%
-    #     stringr::str_split(., '/') %>%
-    #     unlist() %>%
-    #     .[max(length(unlist(.)))] 
-    #   
-    #   pub_date_str <- suppressWarnings(
-    #     suppressMessages(readr::read_csv(x, col_names=c('X1', 'X2')) %>%
-    #                        dplyr::filter(stringr::str_detect(X1, 'Date-Time for Data Publication')))) %>%
-    #     dplyr::mutate(readme_filename = as.factor(split)) %>%
-    #     dplyr::select(-X1, -X2)
-    #   
-    #   return(pub_date_str)
-    # }))
-    txt_file <- readr::read_lines(readme_list[[max(length(readme_list))]])
-    
-    cat("##################################\n", file = out_filepath_name)
-    cat("########### Attention ############\n", file = out_filepath_name, append=TRUE)
-    cat('Disclaimer: this is a compiled version and may have some information from specific site-dates based on readme files used during stackByTable.\n', file = out_filepath_name, append=TRUE)
-    cat("##################################\n", file = out_filepath_name, append=TRUE)
-    cat("\n", file = out_filepath_name, append=TRUE)
-    readr::write_lines(txt_file, out_filepath_name, append=TRUE)
-    cat("\n", file = out_filepath_name, append=TRUE)
-    cat("POST STACKING README DOCUMENTATION\n", file = out_filepath_name, append=TRUE)
-    cat("----------------------------------\n", file = out_filepath_name, append=TRUE)
-    # cat("\n", file = out_filepath_name, append=TRUE)
-    # cat("Each row contains the readme filename used during stackByTable\n", file = out_filepath_name, append=TRUE)
-    # cat("\n", file = out_filepath_name, append=TRUE)
-    # utils::write.table(pub_date_df, file=out_filepath_name, sep=",", append=TRUE, row.names=FALSE, col.names=FALSE, quote = FALSE)
+    return(pub_date_str)
+    }))
+  
+  txt_file <- readr::read_lines(readme_list[[max(length(readme_list))]]) %>%
+    .[!str_detect(., pattern="Date-Time")]
+  
+  cat("##################################\n", file = out_filepath_name)
+  cat("########### Attention ############\n", file = out_filepath_name, append=TRUE)
+  cat('Disclaimer: this is a compiled version and may have some information from specific site-dates based on readme files used during stackByTable.\n', file = out_filepath_name, append=TRUE)
+  cat("##################################\n", file = out_filepath_name, append=TRUE)
+  cat("\n", file = out_filepath_name, append=TRUE)
+  readr::write_lines(txt_file, out_filepath_name, append=TRUE)
+  cat("\n", file = out_filepath_name, append=TRUE)
+  cat("POST STACKING README DOCUMENTATION\n", file = out_filepath_name, append=TRUE)
+  cat("----------------------------------\n", file = out_filepath_name, append=TRUE)
+  cat("\n", file = out_filepath_name, append=TRUE)
+  cat("Each row contains the readme filename used during stackByTable\n", file = out_filepath_name, append=TRUE)
+  cat("\n", file = out_filepath_name, append=TRUE)
+  utils::write.table(pub_date_df, file=out_filepath_name, sep=",", append=TRUE, row.names=FALSE, col.names=FALSE, quote = FALSE)
 }
