956c69ab49013cfee0c8260002db8ae79b1d5f0b
Claire Lunch <clunch@battelleecology.org>
2020-05-26 15:34:10 -0600

    fix syntax in stackByTable() call from loadByProduct()

--- neonUtilities/R/loadByProduct.R
+++ neonUtilities/R/loadByProduct.R
@@ -77,7 +77,8 @@ loadByProduct <- function(dpID, site="all", startdate=NA, enddate=NA, package="b
 
   # stack and load the downloaded files using stackByTable
   out <- stackByTable(filepath=paste(temppath, "/filesToStack", substr(dpID, 5, 9), sep=""),
-                      savepath="envt", folder=TRUE, nCores, forceParallel)
+                      savepath="envt", folder=TRUE, nCores=nCores, 
+                      saveUnzippedFiles=FALSE)
   # Remove temppath directory
   unlink(temppath, recursive=T)
   return(out)
