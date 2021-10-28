2c3147ee678ba87e4a939f04e3fabe5757073b1e
Claire Lunch <clunch@battelleecology.org>
2021-10-28 12:53:15 -0600

    update tests

--- neonUtilities/tests/testthat/test_stackByTable.R
+++ neonUtilities/tests/testthat/test_stackByTable.R
@@ -1,21 +1,21 @@
 context("Table stacking")
 library(neonUtilities)
 
-makes_files <- function(object, output, expected) {
+files_match <- function(object, output, expected) {
   object
-  comp <- compare(list.files(output),
-                  list.files(expected))
-    expect(
-      comp$equal,
-      sprintf("List of stacked files does not match.\n%s", comp$message)
-    )
+  comp <- compare(tools::md5sum(output)[[1]],
+                  tools::md5sum(expected)[[1]])
+  expect(
+    comp$equal,
+    sprintf("Output files do not match previous version files.\n%s", comp$message)
+  )
 }
 
 f <- file.path(tempdir(), "output_gp")
 test_that("Test that correct files are created for microbe group abundances", {
-  makes_files(stackByTable(system.file("extdata", "NEON_gp.zip", 
+  files_match(stackByTable(system.file("extdata", "NEON_gp.zip", 
                                    package="neonUtilities"), savepath=f),
-              paste(f, "stackedFiles", sep="/"),
-              system.file("extdata", "expected/gp", package="neonUtilities"))
+              paste(f, "stackedFiles/mga_soilGroupAbundances.csv", sep="/"),
+              system.file("extdata", "expected/gp/mga_soilGroupAbundances.csv", package="neonUtilities"))
 })
 
