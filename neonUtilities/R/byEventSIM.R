c9d9dcd89b2f00b29517c372dee26fcb90d72d25
Claire Lunch <clunch@neoninc.org>
2025-07-09 17:15:19 -0600

    Update byEventSIM.R

--- neonUtilities/R/byEventSIM.R
+++ neonUtilities/R/byEventSIM.R
@@ -11,11 +11,12 @@
 #' @param site Either the string 'all', meaning all available sites, or a character vector of 4-letter NEON site codes, e.g. c('ONAQ','RMNP'). Defaults to all.
 #' @param startdate Either NA, meaning all available dates, or a character vector in the form YYYY-MM, e.g. 2017-01. Defaults to NA.
 #' @param enddate Either NA, meaning all available dates, or a character vector in the form YYYY-MM, e.g. 2017-01. Defaults to NA.
+#' @param metadata T or F, should metadata files be included in the download? Defaults to TRUE.
 #' @param release The data release to be downloaded; either 'current' or the name of a release, e.g. 'RELEASE-2021'. 'current' returns the most recent release, as well as provisional data if include.provisional is set to TRUE. To download only provisional data, use release='PROVISIONAL'. Defaults to 'current'.
-#' @param include.provisional T or F, should provisional data be included in downloaded files? Defaults to F. See https://www.neonscience.org/data-samples/data-management/data-revisions-releases for details on the difference between provisional and released data.
+#' @param include.provisional T or F, should provisional data be included in downloaded files? Defaults to FALSE. See https://www.neonscience.org/data-samples/data-management/data-revisions-releases for details on the difference between provisional and released data.
 #' @param token User specific API token (generated within data.neonscience.org user accounts)
 
-#' @return A data frame of sim_eventData data, matching the query criteria. Note that metadata are not included in the data returned via this function.
+#' @return A named list containing a data frame of sim_eventData data, matching the query criteria, and, if metadata=TRUE, associated metadata tables such as issue log and citation information. Because this function can retrieve data from any sites and months, the metadata files are retrieved from the most recent data accessed, and the citation file is returned only if a release is specified in the function call.
 
 #' @examples 
 #' \dontrun{
@@ -42,6 +43,7 @@ byEventSIM <- function(eventType,
                        site="all",
                        startdate=NA,
                        enddate=NA,
+                       metadata=TRUE,
                        release="current",
                        include.provisional=FALSE,
                        token=NA_character_) {
@@ -72,6 +74,19 @@ byEventSIM <- function(eventType,
   evds <- dplyr::filter(.data=ds, .data$eventType==evFilter)
   events <- data.frame(dplyr::collect(evds))
   
-  return(events)
+  eventlist <- list()
+  eventlist[[1]] <- events
+  if(isTRUE(metadata)) {
+    tei <- try(eventlist[[length(eventlist)+1]] <- getIssueLog(dpID="DP1.10111.001", 
+                                                               token=token), silent=TRUE)
+    # look at dates in data, get metadata files from most recent dates
+    
+    if(length(grep(pattern="RELEASE", x=release))==1) {
+      ter <- try(eventlist[[length(eventlist)+1]] <- getCitation(dpID="DP1.10111.001", 
+                                                                 release=release), silent=TRUE)
+    }
+  }
+  
+  return(eventlist)
   
 }
