9307e590570427d85e4cba0d552a2a201997efc5
Claire Lunch <clunch@neoninc.org>
2025-07-22 13:20:20 -0600

    update internal test

--- neonUtilities/tests/testthat/test_stackByTable.R
+++ neonUtilities/tests/testthat/test_stackByTable.R
@@ -1,22 +1,13 @@
 context("Table stacking")
 library(neonUtilities)
 
-files_match <- function(object, output, expected) {
-  object
-  comp <- compare(output$uid,
-                  expected$uid)
-  expect(
-    comp$equal,
-    sprintf("Output files do not match previous version files.\n%s", comp$message)
-  )
-}
 
 f <- stackByTable(system.file("extdata", "NEON_litterfall.zip", 
                               package="neonUtilities"), savepath="envt")
+fe <- read.csv(system.file("extdata", "expected/litter/ltr_litterLignin.csv", 
+                     package="neonUtilities"))
 test_that("Test that correct files are created for litterfall", {
-  files_match({},
-              f$ltr_litterLignin,
-              read.csv(system.file("extdata", "expected/litter/ltr_litterLignin.csv", 
-                                   package="neonUtilities")))
+  expect_true(all(f$ltr_litterLignin$uid %in% fe$uid &
+                    all(fe$uid %in% f$ltr_litterLignin$uid)))
 })
 
